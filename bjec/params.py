7c31b667ea31a12e6f095275f4f541309e55227d
Seoester <seoester@googlemail.com>
2017-07-06 21:45:01 +0200

    params: Added Function wrapper class, improvements in documentation

--- bjec/params.py
+++ bjec/params.py
@@ -41,7 +41,7 @@ class P(ParamsEvaluable):
 	Args:
 		key (str): Parameter (key of the parameter in the `params`) dict.
 		f (None or function, optional): If not None, `f` is applied to the
-			value of ``params[key]`` before returning.
+			value of ``params[key]`` and the result is returned.
 	"""
 
 	def __init__(self, key, f=None):
@@ -69,18 +69,27 @@ class P(ParamsEvaluable):
 
 
 class Join(ParamsEvaluable):
-	"""String / Byte Join for lists containing P objects.
+	"""String / Bytes Join for lists containing P objects.
+
+	The type of output is determined by the type of the `sep` argument.
+
+	If the output should be a ``str``, ``str(.)`` will be called on each list
+	element (in `*args`). If the output should be of type ``bytes``, the user
+	has to ensure that each of the list elements are of ``bytes`` type and that
+	``P(.)`` returns a ``bytes`` object.
 
 	Example:
 		::
+
 			Join("out.", P("n"), ".csv")
 
 	Args:
-		*args (object or P, must support str(.)): Elements to join, may be
-			instances of P. ``str()`` is applied to every element before
-			joining.
-		sep (str, optional): Separator used to join elements of `*args`.
-			Defaults to ``""``.
+		*args (object supporting str(.), P or bytes): Elements to join, may
+			be instances of P. If the output type is ``str``, ``str()`` is
+			applied to every element before joining.
+		sep (str or bytes, optional): Separator used to join elements of
+			`*args`. Must have the type of the output, i.e. if the output should
+			be of a ``bytes`` type, `sep` must be as well. Defaults to ``""``.
 	"""
 	def __init__(self, *args, sep=""):
 		self.args = args
@@ -122,6 +131,17 @@ class Factory(ParamsEvaluable):
 
 
 class Function(ParamsEvaluable):
+	"""Wrapper for functions.
+
+	Example:
+		::
+
+			Function(lambda p: p["alpha"] / p["beta"])
+
+	Args:
+		func (function): Function to be called on evaluation. The parameters
+			are passed as the only argument.
+	"""
 	def __init__(self, func):
 		self.func = func
 
