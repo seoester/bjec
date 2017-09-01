76340529033bdef7f93dfb07027a3bd4ead2c092
Seoester <seoester@googlemail.com>
2017-09-01 19:52:34 +0200

    Increased version: 0.2.7 -> 0.2.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
 
 setup(
 	name="bjec",
-	version="0.2.7",
+	version="0.2.8",
 	description="Batch Job Executor & Collector",
 	long_description=long_description,
 	packages=find_packages(),
