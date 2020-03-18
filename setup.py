92e934de21081aa65c782bd6d3331e6b8b966f51
Seoester <seoester@googlemail.com>
2020-03-18 10:54:50 +0100

    Move package to src/ directory

--- setup.py
+++ setup.py
@@ -2,20 +2,23 @@ from setuptools import setup, find_packages
 
 
 with open('README.rst') as file:
-	long_description = file.read()
+    long_description = file.read()
 
 setup(
-	name="bjec",
-	version="0.2.8",
-	description="Batch Job Executor & Collector",
-	long_description=long_description,
-	packages=find_packages(),
-	install_requires=[
-		'argh',
-		'pyyaml',
-		'gitpython',
-	],
-	entry_points={
-		'console_scripts': ['bjec=bjec.cli:main'],
-	},
+    name="bjec",
+    version="0.2.8",
+    description="Batch Job Executor & Collector",
+    long_description=long_description,
+    package_data = {'simulator': ['py.typed']},
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    zip_safe = False,
+    install_requires=[
+        'argh',
+        'pyyaml',
+        'gitpython',
+    ],
+    entry_points={
+        'console_scripts': ['bjec=bjec.cli:main'],
+    },
 )
