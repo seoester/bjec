10a9a4883fb33b9782ba166a8fac2260ddaec694
Seoester <seoester@googlemail.com>
2020-04-07 14:43:55 +0100

    Refactor utils module

--- src/bjec/utils.py
+++ src/bjec/utils.py
@@ -1,25 +1,28 @@
-import collections
 import datetime
+from typing import cast, Iterable, List, Sequence, TypeVar, Union
 
+_T = TypeVar('_T')
 
-def listify(obj, none_empty=False):
-    """listify turns `obj` into an iterable.
+def listify(obj: Union[_T, Sequence[_T], None], none_empty: bool=False) -> List[_T]:
+    """Turns ``obj`` into a list. Returns ``[obj]`` if it.
 
     Returns:
-        `obj` is simply returned, if it already is an iterable.
+        ``obj`` is simply returned, if it already is a list.
         Otherwise - or if it a string - it is wrapped in a list.
-        If `none_empty` is set to ``True``, an empty list is returned, if `obj`
-        is ``None``.
+        If ``none_empty`` is set to ``True``, an empty list is returned, if
+        ``obj`` is ``None``.
     """
+
     if obj is None and none_empty:
         return []
-    elif isinstance(obj, str):
-        return [obj]
-    elif not isinstance(obj, collections.Iterable):
-        return [obj]
-    else:
+    elif isinstance(obj, list):
         return obj
-
+    elif isinstance(obj, (str, bytes)):
+        return [cast(_T, obj)]
+    elif isinstance(obj, Sequence):
+        return list(obj)
+    else:
+        return [cast(_T, obj)]
 
 min_datetime = datetime.datetime.min.replace(tzinfo=datetime.timezone.utc)
 """Minimum representable datetime with timezone ("aware") set to UTC."""
